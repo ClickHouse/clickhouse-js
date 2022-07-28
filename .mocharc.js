46fa67e63da8ff26a5e56a12ffe7a23defa41fd4
restrry <restrry@gmail.com>
2022-07-28 14:15:47 +0200

    remove unnecessary settings

--- .mocharc.js
+++ .mocharc.js
@@ -1,16 +1,5 @@
 module.exports = {
   require: ['chai', 'ts-node/register'],
-
   ui: 'bdd',
-
-  reporter: 'spec', // default
-  // reporter: 'nyan',
-  // reporter: 'tap',
-  // reporter: 'landing',
-  // reporter: 'list'
-  // reporter: 'progress',
-  // reporter: 'min', // clears the terminal
-  // reporter: 'dot',
-
-  growl: false,
+  reporter: 'spec',
 };
