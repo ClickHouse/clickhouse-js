1f06ef74aea34e0608628027561edf3d2866707e
serge.klochkov <hypnoash@gmail.com>
2022-08-08 18:54:22 +0200

    Fix parse_error

--- __tests__/unit/parse_error.ts
+++ __tests__/unit/parse_error.ts
@@ -26,4 +26,16 @@ describe('parseError', () => {
       .equal(`Syntax error: failed at position 15 ('unknown_table') (line 1, col 15): unknown_table
     FORMAT JSON. Expected alias cannot be here. `);
   });
+
+  it('should handle replica message corner case', async () => {
+    const message = `Code: 285. DB::Exception: Number of alive replicas (2) is less than requested quorum (3). (TOO_FEW_LIVE_REPLICAS) (version 22.8.1.11291 (official build))`;
+    const error = parseError(message) as ClickHouseError;
+
+    expect(error).to.be.instanceof(ClickHouseError);
+    expect(error.code).to.equal('285');
+    expect(error.type).to.equal('TOO_FEW_LIVE_REPLICAS');
+    expect(error.message).to.equal(
+      'Number of alive replicas (2) is less than requested quorum (3). '
+    );
+  });
 });
