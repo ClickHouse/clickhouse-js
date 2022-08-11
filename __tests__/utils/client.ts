610b32bda960e995e7ff315693e7ae2c09173d61
serge.klochkov <hypnoash@gmail.com>
2022-08-11 15:18:08 +0200

    Extract test_env.ts, fix unit tests

--- __tests__/utils/client.ts
+++ __tests__/utils/client.ts
@@ -6,12 +6,7 @@ import {
 } from '../../src';
 import { guid } from './guid';
 import { TestLogger } from './test_logger';
-
-export enum TestEnv {
-  Cloud = 'cloud',
-  LocalSingleNode = 'local_single_node',
-  LocalCluster = 'local_cluster',
-}
+import { getClickHouseTestEnvironment, TestEnv } from './test_env';
 
 export function createTestClient(
   config: ClickHouseClientConfigOptions = {}
@@ -81,27 +76,4 @@ function getFromEnv(key: string): string {
     throw Error(`Environment variable ${key} is not set`);
   }
   return value;
-}
-
-export function getClickHouseTestEnvironment(): TestEnv {
-  let env;
-  switch (process.env['CLICKHOUSE_TEST_ENVIRONMENT']) {
-    case 'cloud':
-      env = TestEnv.Cloud;
-      break;
-    case 'local_cluster':
-      env = TestEnv.LocalCluster;
-      break;
-    case 'local_single_node':
-    case undefined:
-      env = TestEnv.LocalSingleNode;
-      break;
-    default:
-      throw new Error(
-        'Unexpected CLICKHOUSE_TEST_ENVIRONMENT value. ' +
-          'Possible options: `local_single_node`, `local_cluster`, `cloud` ' +
-          'or keep it unset to fall back to `local_single_node`'
-      );
-  }
-  return env;
 }
--- __tests__/utils/index.ts
+++ __tests__/utils/index.ts
@@ -3,11 +3,7 @@ import './global_setup';
 
 export { InMemoryLogger } from './in_memory_logger';
 export { TestLogger } from './test_logger';
-export {
-  createTestClient,
-  createRandomDatabase,
-  createTable,
-  getClickHouseTestEnvironment,
-  TestEnv,
-} from './client';
+export { createTestClient, createRandomDatabase, createTable } from './client';
 export { guid } from './guid';
+export { getClickHouseTestEnvironment } from './test_env';
+export { TestEnv } from './test_env';
