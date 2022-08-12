85298c1d5c3c6d4c43ed62cadc36c7b8a8cd6291
restrry <restrry@gmail.com>
2022-08-12 09:06:06 +0200

    use ts-jest preset instead of ts-node

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   testEnvironment: 'node',
+  preset: 'ts-jest',
   clearMocks: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testMatch: ['**/*.test.{js,mjs,ts,tsx}'],
   transform: tsjPreset.transform,
   testTimeout: 30000,
   globals: {
