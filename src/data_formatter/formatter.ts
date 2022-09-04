139168126c023a647e73d835a85863bab6086916
serge.klochkov <hypnoash@gmail.com>
2022-09-05 00:37:19 +0200

    Remove oopsie

--- src/data_formatter/formatter.ts
+++ src/data_formatter/formatter.ts
@@ -93,9 +93,6 @@ export function encode(value: any, format: DataFormat): string {
   //   'JSONCompactStringsEachRowWithNames',
   //   'JSONCompactStringsEachRowWithNamesAndTypes',
   if (format === 'JSONCompactEachRow' || format === 'JSONEachRow') {
-    if (value instanceof Date) {
-      return 'foobar'
-    }
     return JSON.stringify(value) + '\n'
   }
   throw new Error(`The client does not support encoding in [${format}] format.`)
