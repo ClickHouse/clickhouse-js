7301db896a54b60a60d1d2c2b51fb6f45e3b84da
Serge Klochkov <3175289+slvrtrn@users.noreply.github.com>
2022-10-04 17:33:34 +0200

    Update select_streaming_on_data.ts

--- examples/select_streaming_on_data.ts
+++ examples/select_streaming_on_data.ts
@@ -4,7 +4,10 @@ import { createClient } from '../src'
 /**
  * Can be used for consuming large datasets for reducing memory overhead,
  * or if your response exceeds built in Node.js limitations,
- * such as 512Mb for strings
+ * such as 512Mb for strings.
+ *
+ * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length#description
+ * for more information.
  *
  * As `for await const` has quite significant overhead (up to 2 times worse)
  * vs old school `on(data)` approach, this example covers `on(data)` usage
