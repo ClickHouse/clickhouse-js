d03dd070a59a1d216abc8e3a20f613c43605e1a4
slvrtrn <hypnoash@gmail.com>
2023-10-17 14:38:59 +0200

    Add tests

--- packages/client-node/__tests__/integration/node_streaming_e2e.test.ts
+++ packages/client-node/__tests__/integration/node_streaming_e2e.test.ts
@@ -1,6 +1,8 @@
 import type { Row } from '@clickhouse/client-common'
 import { type ClickHouseClient } from '@clickhouse/client-common'
+import { fakerRU } from '@faker-js/faker'
 import { createSimpleTable } from '@test/fixtures/simple_table'
+import { createTableWithFields } from '@test/fixtures/table_with_fields'
 import { createTestClient, guid } from '@test/utils'
 import Fs from 'fs'
 import split from 'split2'
@@ -72,5 +74,121 @@ describe('[Node.js] streaming e2e', () => {
       })
     }
     expect(actual).toEqual(expected)
+  })
+
+  // See https://github.com/ClickHouse/clickhouse-js/issues/171 for more details
+  // Here we generate a large enough dataset to break into multiple chunks while streaming,
+  // effectively testing the implementation of incomplete rows handling
+  describe('should correctly process multiple chunks', () => {
+    async function generateData({
+      maxWords,
+      minWords,
+      rows,
+    }: {
+      rows: number
+      minWords: number
+      maxWords: number
+    }): Promise<{
+      table: string
+      values: { id: number; sentence: string; timestamp: string }[]
+    }> {
+      const table = await createTableWithFields(
+        client as ClickHouseClient,
+        `sentence String, timestamp String`
+      )
+      const values = [...new Array(rows)].map((_, id) => ({
+        id,
+        // it seems that it is easier to trigger an incorrect behavior with non-ASCII symbols
+        sentence: fakerRU.lorem.sentence({ min: minWords, max: maxWords }),
+        timestamp: new Date().toISOString(),
+      }))
+      await client.insert({
+        table,
+        values,
+        format: 'JSONEachRow',
+      })
+      return {
+        table,
+        values,
+      }
+    }
+
+    describe('large amount of rows', () => {
+      fit('should work with .json()', async () => {
+        const { table, values } = await generateData({
+          rows: 10000,
+          minWords: 5,
+          maxWords: 10,
+        })
+        const result = await client
+          .query({
+            query: `SELECT * FROM ${table} ORDER BY id ASC`,
+            format: 'JSONEachRow',
+          })
+          .then((r) => r.json())
+        expect(result).toEqual(values)
+      })
+
+      fit('should work with .stream()', async () => {
+        const { table, values } = await generateData({
+          rows: 10000,
+          minWords: 5,
+          maxWords: 10,
+        })
+        const stream = await client
+          .query({
+            query: `SELECT * FROM ${table} ORDER BY id ASC`,
+            format: 'JSONEachRow',
+          })
+          .then((r) => r.stream())
+
+        const result = []
+        for await (const rows of stream) {
+          for (const row of rows) {
+            result.push(await row.json())
+          }
+        }
+        expect(result).toEqual(values)
+      })
+    })
+
+    describe("rows that don't fit into a single chunk", () => {
+      fit('should work with .json()', async () => {
+        const { table, values } = await generateData({
+          rows: 5,
+          minWords: 5000,
+          maxWords: 10000,
+        })
+        const result = await client
+          .query({
+            query: `SELECT * FROM ${table} ORDER BY id ASC`,
+            format: 'JSONEachRow',
+          })
+          .then((r) => r.json())
+        expect(result).toEqual(values)
+      })
+
+      fit('should work with .stream()', async () => {
+        const { table, values } = await generateData({
+          rows: 5,
+          minWords: 5000,
+          maxWords: 10000,
+        })
+        const stream = await client
+          .query({
+            query: `SELECT * FROM ${table} ORDER BY id ASC`,
+            format: 'JSONEachRow',
+          })
+          .then((r) => r.stream())
+
+        const result = []
+        for await (const rows of stream) {
+          for (const row of rows) {
+            result.push(await row.json())
+          }
+        }
+        expect(result).toEqual(values)
+      })
+    })
   })
 })
