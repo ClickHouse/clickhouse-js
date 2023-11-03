bcac90b0517c2e2bbe89fef90fab482be367b692
slvrtrn <hypnoash@gmail.com>
2023-11-03 20:37:44 +0100

    Update "endless" stream examples

--- examples/node/endless_flowing_stream_raw.ts
+++ examples/node/endless_flowing_stream_raw.ts
@@ -1,77 +1,111 @@
 import { createClient } from '@clickhouse/client'
+import type { InsertResult } from '@clickhouse/client-common'
 import { randomInt } from 'crypto'
+import process from 'node:process'
 import Stream from 'stream'
 
-// Open a single connection for streaming data insertion
-// Periodically push the data into the stream
-// Close the connection only when the application shuts down
+// - Open a single connection at a time for streaming data insertion
+// - Periodically push the data into the stream
+// - Periodically flush the insert stream and recreate it
+//
 // Might be useful for event listeners or some other similar use cases
 //
 // NB: results will not appear in ClickHouse immediately as it is buffered internally,
 // and by default flushing happens very infrequently for smaller datasets,
 // as ClickHouse awaits for the input stream to be terminated.
+// In this scenario, it will happen every 25 seconds.
 //
-// For larger datasets, the data will be flushed more often,
+// For larger datasets, the data will be flushed on the server side more often,
 // as it will exceed the buffer size quicker.
-//
-// To observe the results in ClickHouse, run this program for some time,
-// and then terminate it with Ctrl+C.
-// The data should appear in the table (almost) immediately
-// after the stream is closed
+
 void (async () => {
-  const client = createClient()
-  const tableName = 'endless_flowing_stream_raw'
+  const client = createClient({
+    // should be in sync with http_receive_timeout profile setting
+    request_timeout: 30_000,
+  })
+  const tableName = 'endless_flowing_stream_json'
   await client.command({
     query: `DROP TABLE IF EXISTS ${tableName}`,
   })
   await client.command({
     query: `
       CREATE TABLE ${tableName}
-      (id UInt32, name String)
+      (id UInt64, name String)
       ENGINE MergeTree()
       ORDER BY (id)
     `,
   })
-  const stream = new Stream.Readable({
-    objectMode: false, // required for "raw" family formats
-    read() {
-      //
-    },
-  })
-  // note that we do not await the promise yet
-  const insertPromise = client
-    .insert({
-      table: tableName,
-      values: stream,
-      format: 'CSV',
-    })
-    .then(() => console.info('\nData ingestion is finished'))
 
-  console.info('Starting data ingestion, press Ctrl+C to stop')
-  // Periodically generate some random data and push it into the stream...
-  const timer = setInterval(pushData(stream), 1000)
+  // Note: we do not send the insert request right away.
+  // It will be created only when there is some data to send for the first time.
+  // Otherwise, if the request will have no initial data for `keep_alive_timeout` seconds (ClickHouse server setting),
+  // it will be closed by the server no matter what.
+  let insertFlow:
+    | {
+        stream: Stream.Readable
+        promise: Promise<InsertResult>
+      }
+    | undefined
 
-  // When Ctrl+C is pressed...
-  async function cleanup() {
-    clearInterval(timer)
-    // finally, close the stream
-    stream.push(null)
-    // when the stream is closed, the insert stream can be awaited
-    await insertPromise
-    await client.close()
-    process.exit(0)
-  }
-  process.on('SIGINT', cleanup)
-  process.on('SIGTERM', cleanup)
-})()
-
-function pushData(stream: Stream.Readable) {
-  return async () => {
+  function pushData() {
+    if (insertFlow === undefined) {
+      const stream = new Stream.Readable({
+        objectMode: false, // required for raw (CSV,TSV,etc.) family formats
+        read() {
+          //
+        },
+      })
+      const promise = client.insert({
+        table: tableName,
+        values: stream,
+        format: 'CSV',
+      })
+      insertFlow = {
+        stream,
+        promise,
+      }
+      console.info('Insert stream was (re)created')
+    }
+    console.info('Pushing several records into the stream...')
     console.info('Pushing several records into the stream...')
     ;[...Array(3)].forEach(() => {
       const id = randomInt(1, 100_000_000)
       const name = Math.random().toString(36).slice(2)
-      stream.push(`${id},"${name}"\n`)
+      insertFlow!.stream.push(`${id},"${name}"\n`)
     })
   }
-}
+
+  async function flushInsertStream() {
+    console.info('Flushing the insert stream...')
+    if (insertFlow !== undefined) {
+      // swap the values here to avoid a potential switch to `pushData`
+      // due to awaiting the insert promise when the previous stream was already closed
+      const prevFlow = insertFlow
+      insertFlow = undefined
+      prevFlow.stream.push(null)
+      await prevFlow.promise
+      // insertFlow (stream + insert request) will be recreated on the next `pushData` call
+    }
+  }
+
+  console.info('Starting data ingestion, press Ctrl+C to stop')
+  // Periodically generate some random data and push it into the stream...
+  const pushDataTimer = setInterval(pushData, 1000)
+  // ClickHouse `http_receive_timeout` profile setting is 30s by default
+  // Unfortunately, it cannot be overridden from the client side
+  // So, we need some safe value to periodically flush the insert stream
+  // which will be recreated the next time there is some data to push
+  const flushTimer = setInterval(flushInsertStream, 25_000)
+
+  // When Ctrl+C is pressed...
+  async function cleanup() {
+    clearInterval(pushDataTimer)
+    clearInterval(flushTimer)
+    await flushInsertStream()
+    await client.close()
+    process.exit(0)
+  }
+
+  process.on('SIGINT', cleanup)
+  process.on('SIGTERM', cleanup)
+})()
