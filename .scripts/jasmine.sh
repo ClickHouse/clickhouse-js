7b0ce66525ee1f58b8b6a94c99a2114a68bf060d
slvrtrn <hypnoash@gmail.com>
2024-03-29 00:47:52 +0100

    Coverage reports for Node.js package

--- .scripts/jasmine.sh
+++ .scripts/jasmine.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ts-node -r tsconfig-paths/register --transpileOnly --project=tsconfig.dev.json node_modules/jasmine/bin/jasmine --config=$1
+ts-node -r tsconfig-paths/register -r source-map-support/register --transpileOnly --project=tsconfig.dev.json node_modules/jasmine/bin/jasmine --config=$1
