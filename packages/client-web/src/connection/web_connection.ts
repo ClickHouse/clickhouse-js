7fe20e43dd91f5b353d9f0280c956d36a38db22b
Serge Klochkov <3175289+slvrtrn@users.noreply.github.com>
2024-10-21 16:20:00 +0200

    Revert "Added missing crypto import (#341)" (#342)

--- packages/client-web/src/connection/web_connection.ts
+++ packages/client-web/src/connection/web_connection.ts
@@ -18,7 +18,6 @@ import {
   withHttpSettings,
 } from '@clickhouse/client-common'
 import { getAsText } from '../utils'
-import crypto from 'crypto'
 
 type WebInsertParams<T> = Omit<
   ConnInsertParams<ReadableStream<T>>,
