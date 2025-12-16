fec83303194945f0486db866bd914b325b342eb9
Peter Leonov <peter.leonov@clickhouse.com>
2025-12-16 21:00:26 +0100

    add unit tests for the client dispose method

---
+++ packages/client-node/__tests__/utils/feture_detection.ts
@@ -0,0 +1,12 @@
+export function isAwaitUsingStatementSupported(): boolean {
+  try {
+    eval(`
+      (async () => {
+          await using c = null;
+      })
+    `)
+    return true
+  } catch {
+    return false
+  }
+}
