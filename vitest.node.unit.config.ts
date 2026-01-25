2da42a349ca69ed8b2709ac215b2b4fa099b6a41
Peter Leonov <peter.leonov@clickhouse.com>
2026-01-25 12:54:32 +0100

    use source directly

--- vitest.node.unit.config.ts
+++ vitest.node.unit.config.ts
@@ -5,4 +5,9 @@ export default defineConfig({
     root: 'packages/client-node/__tests__',
     include: ['unit/*.test.ts', 'utils/*.test.ts'],
   },
+  resolve: {
+    alias: {
+      '@clickhouse/client-common': '../../client-common/src',
+    },
+  },
 })
