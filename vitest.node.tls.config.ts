8ef46dba5dc3670610ddbfd3ac5c102689df91cd
Peter Leonov <peter.leonov@clickhouse.com>
2026-02-08 22:48:53 +0100

    fix npm run test:node:tls

--- vitest.node.tls.config.ts
+++ vitest.node.tls.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    // Increase maxWorkers to speed up integration tests
+    // as we're not bound by the CPU here.
+    maxWorkers: '400%',
     globals: true,
     root: 'packages/client-node/__tests__',
     include: ['tls/*.test.ts'],
+    setupFiles: ['../../../vitest.node.setup.ts'],
   },
   resolve: {
     alias: {
