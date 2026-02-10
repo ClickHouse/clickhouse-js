1fbc37c7dce1344e1730b761ec568723234f97b4
Peter Leonov <peter.leonov@clickhouse.com>
2026-02-10 14:11:54 +0100

    run all test in one go

---
+++ vitest.node.all.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    // Increase maxWorkers to speed up integration tests
+    // as we're not bound by the CPU here.
+    maxWorkers: '400%',
+    // Cover the Cloud instance wake-up time
+    testTimeout: 300_000,
+    slowTestThreshold: 10_000,
+    globals: true,
+    include: [
+      'packages/client-common/__tests__/unit/*.test.ts',
+      'packages/client-common/__tests__/utils/*.test.ts',
+      'packages/client-common/__tests__/integration/*.test.ts',
+      'packages/client-node/__tests__/tls/*.test.ts',
+      'packages/client-node/__tests__/unit/*.test.ts',
+      'packages/client-node/__tests__/utils/*.test.ts',
+      'packages/client-node/__tests__/integration/*.test.ts',
+    ],
+    setupFiles: ['vitest.node.setup.ts'],
+    coverage: {
+      provider: 'istanbul',
+    },
+  },
+  resolve: {
+    alias: {
+      '@clickhouse/client-common': 'packages/client-common/src',
+      '@clickhouse/client-node': 'packages/client-node/src',
+      '@test': 'packages/client-common/__tests__',
+    },
+  },
+})
