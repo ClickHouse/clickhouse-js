d79642fe75af5ed15464c3a1aadb76a1858da426
Peter Leonov <peter.leonov@clickhouse.com>
2026-02-10 16:55:22 +0100

    wip1

--- vitest.web.setup.ts
+++ vitest.web.setup.ts
@@ -5,3 +5,9 @@ import { createClient } from '@clickhouse/client-web'
  * This file is used to set up the test environment for Vitest when running tests in Node.js.
  */
 globalThis.environmentSpecificCreateClient = createClient
+
+globalThis.process = {
+  env: {
+    BROWSER: 'chromium',
+  },
+}
