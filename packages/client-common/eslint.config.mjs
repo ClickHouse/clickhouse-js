d9faa4632aa18ab1693d26b848271fb9e8d73d0f
Peter Leonov <peter.leonov@clickhouse.com>
2026-02-13 12:24:04 +0100

    fix eslint in VS Code

--- packages/client-common/eslint.config.mjs
+++ packages/client-common/eslint.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig(
     languageOptions: {
       parserOptions: {
         projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
