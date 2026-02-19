abb209c3a6ade01153babf79b87f90127ede84b5
Peter Leonov <peter.leonov@clickhouse.com>
2026-02-19 09:54:26 +0100

    typo

--- packages/client-node/src/connection/stream.ts
+++ packages/client-node/src/connection/stream.ts
@@ -27,7 +27,7 @@ export async function drainStream(stream: Stream.Readable): Promise<void> {
       stream.removeListener('data', dropData)
       stream.removeListener('end', onEnd)
       stream.removeListener('error', onError)
-      stream.removeListener('onClose', onClose)
+      stream.removeListener('close', onClose)
     }
 
     stream.on('data', dropData)
