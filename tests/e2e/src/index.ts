c6c4fd80aff3e4c4f99f4bac2d50e0946b04a1b8
Peter Leonov <peter.leonov@clickhouse.com>
2026-02-25 16:02:43 +0100

    check some invariants

--- tests/e2e/src/index.ts
+++ tests/e2e/src/index.ts
@@ -1,4 +1,28 @@
+const assert = require('assert')
 const { createClient } = require('@clickhouse/client')
-const { version } = require('@clickhouse/client/version')
-console.log(version)
-console.log(typeof createClient())
+const version = require('@clickhouse/client/dist/version')
+
+async function main() {
+  const tags = await (
+    await fetch(
+      'https://registry.npmjs.org/-/package/@clickhouse/client/dist-tags',
+    )
+  ).json()
+
+  console.log(`Latest beta version on npm: ${tags.beta}`)
+
+  assert.strictEqual(
+    version.default,
+    tags.beta,
+    'Version should be the latest beta version on npm',
+  )
+
+  assert.strictEqual(
+    typeof createClient,
+    'function',
+    'createClient should be a function',
+  )
+  assert.ok(createClient(), 'createClient should return a client instance')
+}
+
+main()
