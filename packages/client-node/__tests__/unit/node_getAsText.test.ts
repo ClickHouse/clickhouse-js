74aefd6755e8394d9f6f73adc87e1e1b5b1f8122
Peter Leonov <peter.leonov@clickhouse.com>
2026-03-12 19:09:26 +0100

    trigger with striings

--- packages/client-node/__tests__/unit/node_getAsText.test.ts
+++ packages/client-node/__tests__/unit/node_getAsText.test.ts
@@ -1,18 +1,41 @@
 import { describe, expect, it } from 'vitest'
 import Stream from 'stream'
+import { constants } from 'buffer'
+
 import { getAsText } from '../../src/utils/stream'
 
-function makeRowsStream() {
+function makeRowsStream(chunks: string[]): Stream.Readable {
   const encoder = new TextEncoder()
   async function* gen() {
-    yield encoder.encode('1')
-    yield encoder.encode('2')
+    for (const chunk of chunks) {
+      yield encoder.encode(chunk)
+    }
   }
   return Stream.Readable.from(gen(), { objectMode: false })
 }
 
 describe('getAsText', () => {
-  it('should return a string', async () => {
-    expect(await getAsText(makeRowsStream())).toBe('12')
+  it('should return a string containing the concatenated chunks', async () => {
+    expect(await getAsText(makeRowsStream(['123', '456']))).toBe('123456')
+  })
+
+  it('should throw a custom error if the stream is too long for a string', async () => {
+    const longString =
+      '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'.repeat(
+        1000,
+      )
+    console.log('!!!!!!!!!!!!!!!!!!!!')
+    console.log(constants.MAX_STRING_LENGTH)
+    console.log(longString.length)
+    console.log('!!!!!!!!!!!!!!!!!!!!')
+    const longChunk = longString.repeat(
+      constants.MAX_STRING_LENGTH / longString.length / 2,
+    )
+
+    expect(
+      getAsText(makeRowsStream([longChunk, longChunk, longChunk, longChunk])),
+    ).rejects.toThrowError(
+      'The response length exceeds the maximum allowed size of V8 String:',
+    )
   })
 })
