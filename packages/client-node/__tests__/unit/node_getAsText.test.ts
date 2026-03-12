41f1316529ba6ec38537f4f838456a5951bc94c3
Peter Leonov <peter.leonov@clickhouse.com>
2026-03-12 19:25:19 +0100

    faster with empty buffers

--- packages/client-node/__tests__/unit/node_getAsText.test.ts
+++ packages/client-node/__tests__/unit/node_getAsText.test.ts
@@ -4,7 +4,7 @@ import { constants } from 'buffer'
 
 import { getAsText } from '../../src/utils/stream'
 
-function makeRowsStream(chunks: string[]): Stream.Readable {
+function makeStreamFromStrings(chunks: string[]): Stream.Readable {
   const encoder = new TextEncoder()
   async function* gen() {
     for (const chunk of chunks) {
@@ -14,26 +14,39 @@ function makeRowsStream(chunks: string[]): Stream.Readable {
   return Stream.Readable.from(gen(), { objectMode: false })
 }
 
+function makeStreamFromBuffers(chunks: Buffer[]): Stream.Readable {
+  async function* gen() {
+    for (const chunk of chunks) {
+      yield chunk
+    }
+  }
+  return Stream.Readable.from(gen(), { objectMode: false })
+}
+
 describe('getAsText', () => {
   it('should return a string containing the concatenated chunks', async () => {
-    expect(await getAsText(makeRowsStream(['123', '456']))).toBe('123456')
+    expect(await getAsText(makeStreamFromStrings(['123', '456']))).toBe(
+      '123456',
+    )
   })
 
   it('should throw a custom error if the stream is too long for a string', async () => {
-    const longString =
-      '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890'.repeat(
-        1000,
-      )
-    console.log('!!!!!!!!!!!!!!!!!!!!')
-    console.log(constants.MAX_STRING_LENGTH)
-    console.log(longString.length)
-    console.log('!!!!!!!!!!!!!!!!!!!!')
-    const longChunk = longString.repeat(
-      constants.MAX_STRING_LENGTH / longString.length / 2,
-    )
-
-    expect(
-      getAsText(makeRowsStream([longChunk, longChunk, longChunk, longChunk])),
+    const bigChunk = Buffer.alloc((constants.MAX_STRING_LENGTH / 8) >> 0)
+    await expect(
+      getAsText(
+        makeStreamFromBuffers([
+          bigChunk,
+          bigChunk,
+          bigChunk,
+          bigChunk,
+          bigChunk,
+          bigChunk,
+          bigChunk,
+          bigChunk,
+          bigChunk,
+          bigChunk,
+        ]),
+      ),
     ).rejects.toThrowError(
       'The response length exceeds the maximum allowed size of V8 String:',
     )
