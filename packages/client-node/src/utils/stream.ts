45a629a31b2fabb973633938ed51c94f9363c480
Peter Leonov <peter.leonov@clickhouse.com>
2026-03-13 19:51:29 +0100

    use array.join()

--- packages/client-node/src/utils/stream.ts
+++ packages/client-node/src/utils/stream.ts
@@ -15,27 +15,36 @@ export function isStream(obj: unknown): obj is Stream.Readable {
 }
 
 export async function getAsText(stream: Stream.Readable): Promise<string> {
-  let text = ''
+  const text = []
+  let length = 0
 
   const textDecoder = new TextDecoder()
   for await (const chunk of stream) {
     const decoded = textDecoder.decode(chunk, { stream: true })
-    if (decoded.length + text.length > MAX_STRING_LENGTH) {
+    text.push(decoded)
+    length += decoded.length
+    if (length > MAX_STRING_LENGTH) {
       throw new Error(
         'The response length exceeds the maximum allowed size of V8 String: ' +
           `${MAX_STRING_LENGTH}; consider limiting the amount of requested rows.`,
       )
     }
-    text += decoded
   }
 
   // flush
   const last = textDecoder.decode()
   if (last) {
-    text += last
+    text.push(last)
+    length += last.length
+    if (length > MAX_STRING_LENGTH) {
+      throw new Error(
+        'The response length exceeds the maximum allowed size of V8 String: ' +
+          `${MAX_STRING_LENGTH}; consider limiting the amount of requested rows.`,
+      )
+    }
   }
 
-  return text
+  return text.join('')
 }
 
 export function mapStream(
