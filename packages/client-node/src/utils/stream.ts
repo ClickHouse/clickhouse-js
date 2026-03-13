e79732b0534c766d544d0a235539a8a70e64fe00
Peter Leonov <peter.leonov@clickhouse.com>
2026-03-13 20:06:45 +0100

    rely on the built-in exception

--- packages/client-node/src/utils/stream.ts
+++ packages/client-node/src/utils/stream.ts
@@ -15,35 +15,35 @@ export function isStream(obj: unknown): obj is Stream.Readable {
 }
 
 export async function getAsText(stream: Stream.Readable): Promise<string> {
-  let text = ''
+  try {
+    let text = ''
 
-  const textDecoder = new TextDecoder()
-  for await (const chunk of stream) {
-    const decoded = textDecoder.decode(chunk, { stream: true })
-    if (text.length + decoded.length > MAX_STRING_LENGTH) {
+    const textDecoder = new TextDecoder()
+    for await (const chunk of stream) {
+      const decoded = textDecoder.decode(chunk, { stream: true })
+      text += decoded
+    }
+
+    // flush
+    const last = textDecoder.decode()
+    if (last) {
+      text += last
+    }
+
+    // Single allocation of the resulting string, should increase memory locality
+    // and reduce GC pressure for cons-strings. See https://v8.dev/blog/strings#cons-strings for more details.
+    return text
+  } catch (err) {
+    if (
+      err instanceof RangeError &&
+      err.message.includes('Invalid string length')
+    ) {
       throw new Error(
-        'The response length exceeds the maximum allowed size of V8 String: ' +
-          `${MAX_STRING_LENGTH}; consider limiting the amount of requested rows.`,
+        `The response length exceeds the maximum allowed size of V8 String: ${MAX_STRING_LENGTH} characters.`,
       )
     }
-    text += decoded
+    throw err
   }
-
-  // flush
-  const last = textDecoder.decode()
-  if (last) {
-    text += last
-    if (text.length > MAX_STRING_LENGTH) {
-      throw new Error(
-        'The response length exceeds the maximum allowed size of V8 String: ' +
-          `${MAX_STRING_LENGTH}; consider limiting the amount of requested rows.`,
-      )
-    }
-  }
-
-  // Single allocation of the resulting string, should increase memory locality
-  // and reduce GC pressure for cons-strings. See https://v8.dev/blog/strings#cons-strings for more details.
-  return text
 }
 
 export function mapStream(
