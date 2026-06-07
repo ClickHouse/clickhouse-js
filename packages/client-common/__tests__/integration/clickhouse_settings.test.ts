195cb78f370e53c76f0530b60933d7f17ebc477a
lmlm <7487674+popsiclelmlm@users.noreply.github.com>
2026-06-07 15:38:15 +0800

    test(common): cover enum ClickHouse settings

--- packages/client-common/__tests__/integration/clickhouse_settings.test.ts
+++ packages/client-common/__tests__/integration/clickhouse_settings.test.ts
@@ -1,10 +1,56 @@
 import { describe, it, expect, beforeEach, afterEach } from 'vitest'
-import type { ClickHouseClient, InsertParams } from '@clickhouse/client-common'
+import type {
+  ClickHouseClient,
+  ClickHouseSettings,
+  InsertParams,
+} from '@clickhouse/client-common'
 import { SettingsMap } from '@clickhouse/client-common'
 import { createSimpleTable } from '../fixtures/simple_table'
 import { createTestClient, guid } from '../utils'
 
-// TODO: cover at least all enum settings
+const enumSettings = [
+  ['date_time_input_format', 'best_effort'],
+  ['date_time_output_format', 'iso'],
+  ['default_table_engine', 'Memory'],
+  ['default_temporary_table_engine', 'Memory'],
+  ['dialect', 'clickhouse'],
+  ['distinct_overflow_mode', 'break'],
+  ['distributed_ddl_output_mode', 'none'],
+  ['distributed_product_mode', 'global'],
+  ['except_default_mode', 'ALL'],
+  ['format_capn_proto_enum_comparising_mode', 'by_names'],
+  ['format_custom_escaping_rule', 'Raw'],
+  ['format_regexp_escaping_rule', 'Raw'],
+  ['group_by_overflow_mode', 'any'],
+  ['intersect_default_mode', 'DISTINCT'],
+  ['interval_output_format', 'kusto'],
+  ['join_algorithm', 'hash'],
+  ['join_default_strictness', 'ANY'],
+  ['join_overflow_mode', 'break'],
+  ['load_balancing', 'round_robin'],
+  ['log_queries_min_type', 'EXCEPTION_WHILE_PROCESSING'],
+  ['mysql_datatypes_support_level', 'decimal'],
+  ['output_format_arrow_compression_method', 'none'],
+  ['output_format_msgpack_uuid_representation', 'str'],
+  ['output_format_orc_compression_method', 'none'],
+  ['output_format_parquet_compression_method', 'none'],
+  ['output_format_parquet_version', '2.6'],
+  ['parallel_replicas_custom_key_filter_type', 'default'],
+  ['read_overflow_mode', 'break'],
+  ['read_overflow_mode_leaf', 'break'],
+  ['result_overflow_mode', 'break'],
+  ['send_logs_level', 'fatal'],
+  ['set_overflow_mode', 'break'],
+  ['short_circuit_function_evaluation', 'enable'],
+  ['sort_overflow_mode', 'break'],
+  ['storage_file_read_method', 'read'],
+  ['timeout_overflow_mode', 'break'],
+  ['totals_mode', 'after_having_inclusive'],
+  ['transfer_overflow_mode', 'break'],
+  ['union_default_mode', 'ALL'],
+  ['wait_changes_become_visible_after_commit_mode', 'async'],
+] satisfies Array<[keyof ClickHouseSettings, string]>
+
 describe('ClickHouse settings', () => {
   let client: ClickHouseClient
   beforeEach(() => {
@@ -28,6 +74,24 @@ describe('ClickHouse settings', () => {
       .then((r) => r.text())
     expect(result).toEqual('0\n1\n2\n4\n5\n')
   })
+
+  it.each(enumSettings)(
+    'should work with enum setting %s',
+    async (name, value) => {
+      const clickhouse_settings: ClickHouseSettings = {
+        [name]: value,
+      }
+      const result = await client
+        .query({
+          query: `SELECT toString(getSetting('${name}')) AS value`,
+          format: 'JSONEachRow',
+          clickhouse_settings,
+        })
+        .then((r) => r.json<{ value: string }>())
+
+      expect(result).toEqual([{ value }])
+    },
+  )
 
   // covers both command and insert settings behavior
   // `insert_deduplication_token` will not work without
